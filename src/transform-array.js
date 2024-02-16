528f1e6df6a9768e88b903488ef6aa853ee394b9
Pavel <verbipo@gmail.com>
2024-02-16 19:30:51 +0100

    feat: array transform  what season

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+
+  const answerArr = [];
+
+  arr.forEach((element, index, array) => {
+    if (answerArr[index - 1] === "skipped") {
+      return;
+    }
+
+    switch (element) {
+      case "--double-next":
+        if (index < arr.length && array[index + 1]) {
+          answerArr.push(array[index + 1]);
+        }
+        break;
+
+      case "--double-prev":
+        if (array[index - 1]) {
+          answerArr.push(array[index - 1]);
+        }
+        break;
+
+      case "--discard-next":
+        answerArr.push("skipped");
+        answerArr.push("skipped");
+        break;
+
+      case "--discard-prev":
+        if (index >= 1) {
+          answerArr.splice(index - 1, 1);
+        }
+        break;
+
+      default:
+        if (!answerArr[index]) {
+          answerArr.push(element);
+        }
+        break;
+    }
+  });
+
+  return answerArr.filter((v) => v !== "skipped");
 }
+
 
 module.exports = {
   transform
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (typeof date === "undefined") {
+    return "Unable to determine the time of year!";
+  }
+  if (!date || typeof date.getMonth !== "function") {
+    throw new Error("Invalid date!");
+  }
+
+  let month = -1;
+
+  try {
+    month = date.getMonth();
+    const isDate = date instanceof Date && isNaN(date);
+  } catch (e) {
+    throw new Error("Invalid date!");
+  }
+
+  switch (month) {
+    case 11:
+    case 0:
+    case 1:
+      return "winter";
+
+    case 2:
+    case 3:
+    case 4:
+      return "spring";
+
+    case 5:
+    case 6:
+    case 7:
+      return "summer";
+
+    case 8:
+    case 9:
+    case 10:
+      return "autumn";
+
+    default:
+      return "Unable to determine the time of year!";
+  }
 }
 
 module.exports = {
