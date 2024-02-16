2875c6bd66973064560d5d55728a0e526085d0f4
Pavel <verbipo@gmail.com>
2024-02-16 19:12:42 +0100

    feat: repiter, hanoi tower

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function repeater(str, options) {
+  const {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|',
+  } = options;
 
+  const strWithAddition = str + new Array(additionRepeatTimes).fill(String(addition)).join(additionSeparator);
+  return new Array(repeatTimes).fill(strWithAddition).join(separator);
+}
 module.exports = {
   repeater
 };
